b3ca24e5a7e7dd79e6f645951e323998efb3567d
Aang Ardiansyah-XD <92802033+AngCyber@users.noreply.github.com>
2021-11-14 03:42:18 +0700

    Add files via upload

--- MemekMantan.py
+++ MemekMantan.py
@@ -100,7 +100,7 @@ def masuk():
         if nanya in(""):
         	print ("%s [!] saya bertanya wajib di jawab "%(M));jeda(2);masuk()
         elif nanya in("y","Y"):
-        	print ("\n%s [√] selamat anda pintar :* "%(M));jeda(2);os.system("xdg-open https://youtu.be/iDVCcnLcTnE");masuk()
+        	print ("\n%s [√] selamat anda pintar :* ");masuk()
         elif nanya in("n","N"):
         	print ("\n%s [!] anda sungguh tolol,goblok,dan ngentod!! "%(M));jeda(2);os.system("xdg-open https://youtu.be/iDVCcnLcTnE");masuk()
     elif rom in ('0', '00'):
