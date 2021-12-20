c74c3fa309bd09980f92d49fd01ebcd0d9bc4ec6
Aang Ardiansyah-XD <92802033+AngCyber@users.noreply.github.com>
2021-12-21 04:37:52 +0700

    Add files via upload

--- people.py
+++ people.py
@@ -75,9 +75,9 @@ def banner():
 header = {"x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), "x-fb-sim-hni": str(random.randint(20000, 40000)), "x-fb-net-hni": str(random.randint(20000, 40000)), "x-fb-connection-quality": "EXCELLENT", "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA", "user-agent": "NokiaC3-00/5.0 (07.20) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+ ;]", "content-type": "application/x-www-form-urlencoded", "x-fb-http-engine": "Liger"}
 def masuk():
     os.system('clear');banner()
-    print ('%s╔═[1] Login pakai token \n╠═[2] Cara mendapatkan token \n╠═[%s0%s] Keluar (Ahh Ngecrot)'%(K,M,K))
+    print ('%s╔═[%s1%s]%s Login pakai token \n%s╠═[%s2%s]%s Cara mendapatkan token \n╠═[%s0%s] Keluar (Ahh Ngecrot)'%(K,M,K))
     print ('%s║%s'%(K,P))
-    rom = raw_input('%s╚═[•] Menu : %s'%(P,K))
+    rom = raw_input('%s╚═[•] Menu : %s'%(K,P))
     if rom in(""):
     	print("%s╚═[•] Isi yang benar kentod ] "%(M));exit()
     elif rom in ('1','01'):
